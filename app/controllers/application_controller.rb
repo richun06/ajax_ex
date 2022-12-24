6c4fd2593a85bc28f880a2c497ffe0ce35b8307e
rika kaminoyama <richun.06@gmail.com>
2022-12-24 21:45:08 +0900

    diveseのストロングパラメータ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # deviseのコントローラはgem本体にあるため、ストロングパラメータはapplication_controllerに設定
+
+  before_action :configure_permitted_parameters, if: :devise_controller? # if: :devise_controller?によって、deviseコントローラのみでの制御になる
+
+  protected
+
+  # deviseが初期で作るカラムの他に追加した場合、パラメータに入れる処理
+  def configure_permitted_parameters # configure_permitted_parameters deviseのストロングパラメータ設定の専用のメソッド
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
