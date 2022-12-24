39cc590940927d3c38a24aecfbae0f05975da07d
rika kaminoyama <richun.06@gmail.com>
2022-12-24 23:45:03 +0900

    コメントの編集機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
+  before_action :set_blog, only: [:create, :edit, :update]
 
   # コメントを保存、投稿するためのアクション
   def create
-    @blog = Blog.find(params[:blog_id])
+    # @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.build(comment_params) # Blogをパラメータの値から探し出し、Blogに紐づくcommentsとしてbuildする
     # @blog.comments.build(comment_params)という記述をすることで、@blogのidをblog_idにあらかじめ含んだ状態のCommentインスタンスをnew（作成）することができる
 
@@ -17,9 +18,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = @blog.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+  def update
+    @comment = @blog.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit }
+        end
+      end
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
   end
 end
