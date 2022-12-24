994ec2d1eb7be69fe9c8b942ea431e9f48cc1512
rika kaminoyama <richun.06@gmail.com>
2022-12-24 22:36:45 +0900

    jQueryが使えるようにマニフェストファイルの設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,22 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/XXXXXXXXXXXXXXXXXXXXXX) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require jquery/dist/jquery.js
+//= require_tree .
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
