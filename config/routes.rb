9e627c8dd2612434adbb1807417bb53f9f9e2689
rika kaminoyama <richun.06@gmail.com>
2022-12-24 21:26:20 +0900

    deviseのメール設定をする

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
