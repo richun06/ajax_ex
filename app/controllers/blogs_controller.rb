180ba8d3815e61aec34f12607ee558bc9c3a0168
rika kaminoyama <richun.06@gmail.com>
2022-12-24 21:39:10 +0900

    deviseで使えるヘルパーメソッド

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: [:new, :create] # ログインユーザのみがブログ投稿できる、未ログインでも一覧表示は可能
 
   # GET /blogs or /blogs.json
   def index
