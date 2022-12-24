a63af50dac8ffff3c6358d65cd2fdc4236ae8ddc
rika kaminoyama <richun.06@gmail.com>
2022-12-24 23:23:03 +0900

    コメントが画面ロードなしで反映できるようJSファイルを作成し設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,8 @@ class CommentsController < ApplicationController
     # byebug
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to blog_path(@blog) }
+        # format.html { redirect_to blog_path(@blog) }
+        format.js { render :index }
       else
         format.html { redirect_to blog_path(@blog), notice: '投稿できませんでした...' }
       end
