fc13847d577e0af9548bf459ae77e1a37aa5e9fe
rika kaminoyama <richun.06@gmail.com>
2022-12-25 21:07:02 +0900

    ブログ投稿時にuser_idを渡すよう記述

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -26,6 +26,7 @@ class BlogsController < ApplicationController
   # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
 
     respond_to do |format|
       if @blog.save
@@ -69,6 +70,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title, :content)
+      params.require(:blog).permit(:title, :content, :user_id)
     end
 end
