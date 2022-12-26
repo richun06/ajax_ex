bd163fcbb59db5673a470797665e79b8d90e2e86
rika kaminoyama <richun.06@gmail.com>
2022-12-27 07:40:25 +0900

    どの会話でされているメッセージなのかを取得する処理を共通処理として記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,7 @@
 class MessagesController < ApplicationController
+  # メッセージの機能の場合、いずれのアクションも「どの会話（チャットルーム）に存在するメッセージなのか？」という情報が必要不可欠のため、
+  # 「どの会話（チャットルーム）でなされているメッセージなのか？」を取得する処理を各アクションではなく共通の処理としてコントローラに定義する
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
+  end
 end
