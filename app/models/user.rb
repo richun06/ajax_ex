cb07581c8d9c1c8881a0b1076a092d7938047635
rika kaminoyama <richun.06@gmail.com>
2022-12-27 06:54:51 +0900

    フォロー機能に必要なメソッドを定義する

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,14 @@ class User < ApplicationRecord
   # 中間テーブルを介してレコードを取得できるように
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  #指定のユーザをフォローする
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+
+  #フォローしているかどうかを確認する
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
 end
