c968a9b33de4043151bcb9e183bcfa3a49c6aa7e
rika kaminoyama <richun.06@gmail.com>
2022-12-27 06:53:56 +0900

    フォローの処理(create)を実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,18 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  respond_to? :js # 存在するアクションのrespondを全てjsで返す場合はこのような記述でも可能
+
   def create
+    # 自身で作ったログイン機能であれば、独自実装してあるはずのlogged_in?メソッドを使用して、ログイン時のみフォローできるようにする
+    # if logged_in?
+    #   @user = User.find(params[:relationship][:followed_id])
+    #   current_user.follow!(@user)
+    # end
+
+    # フォローしたいユーザを取得
+    @user = User.find(params[:relationship][:followed_id])
+    # そのユーザをフォローする
+    current_user.follow!(@user)
   end
 
   def destroy
