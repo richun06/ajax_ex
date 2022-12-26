7af363855ae8c883bd72b667d97157a42a41b2a4
rika kaminoyama <richun.06@gmail.com>
2022-12-27 06:37:38 +0900

    Userモデルにアソシエーションの定義をする

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
 
   has_many :blogs, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  # 自分が「フォロワーである」という関係性を取り出すアソシエーション
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  # 自分が「フォローされている側である」という関係性を取り出すアソシエーション
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+
 end
