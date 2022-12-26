5b534330e5f0828fd8275e07991a67e10f62c8c8
rika kaminoyama <richun.06@gmail.com>
2022-12-27 07:25:01 +0900

    新たな会話を作成するアクション を実装

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,4 +2,24 @@ class ConversationsController < ApplicationController
   def index
     @conversations = Conversation.all
   end
+
+  # senderが送り主で、recipientが受取人
+  def create
+    # 該当のユーザ間での会話が過去に存在しているか（該当ユーザー間で会話をするためのチャットルームがすでに存在しているか）を確認する式
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?
+      # 存在していたら、その会話（チャットルーム）情報を取得する
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    else # もし、過去に一件も存在しなかった場合、
+      # 強制的に会話（チャットルーム）情報を生成する
+      @conversation = Conversation.create!(conversation_params)
+    end
+    # いずれの状態であってもその後その会話に紐づくメッセージの一覧画面へ遷移させる
+    redirect_to conversation_messages_path(@conversation)
+  end
+
+  private
+
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
 end
