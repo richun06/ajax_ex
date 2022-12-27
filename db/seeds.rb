d54b257fc1e1b775468c9f6cd91d0074c7c0a9af
rika kaminoyama <richun.06@gmail.com>
2022-12-28 00:10:20 +0900

    名前とメールアドレスをFakerを使ってダミーデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  User.create!(name: Faker::JapaneseMedia::StudioGhibli.unique.character,
+    email: Faker::Internet.email,
+    password: "111111",
+    password_confirmation: "111111"
+  )
+end
