85758d7a68d40ca9c04cc2fdc261b3f393aa6e2f
rika kaminoyama <richun.06@gmail.com>
2022-12-27 23:25:08 +0900

    手動でモデルのみにスキーマ情報を表示させる

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,3 +2,23 @@ class Blog < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
 end
+
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_blogs_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,3 +3,25 @@ class Comment < ApplicationRecord
   validates :content, presence: true
   belongs_to :user
 end
+
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  blog_id    :bigint           not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_comments_on_blog_id  (blog_id)
+#  index_comments_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (blog_id => blogs.id)
+#  fk_rails_...  (user_id => users.id)
+#
