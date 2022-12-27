a273c700d202cb5e835941593081c6f97c7a5661
rika kaminoyama <richun.06@gmail.com>
2022-12-28 00:12:27 +0900

    db:seedの前にdb:migrate:resetをするとエラーになりdb:rollback、db:migrateをするとエラー、該当マイグレーションファイルがdown状態になったため削除しdb:migrateをすると成功

--- db/migrate/20221227143509_drop_posts.rb
+++
@@ -1,9 +0,0 @@
-class DropPosts < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :posts do |t|
-      t.string :title
-      t.datetime :created_at, precision: 6, null: false
-      t.datetime :updated_at, precision: 6, null: false
-    end
-  end
-end
